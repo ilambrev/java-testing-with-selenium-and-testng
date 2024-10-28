8a4a52fd67fb06c662ff887667ca51b42ec52f79
ilambrev <ilambrev@abv.bg>
2024-10-28 10:06:48 +0200

    LoginPage and ProductPage classes created

---
+++ page-object-model-design-pattern/src/main/java/pages/LoginPage.java
@@ -0,0 +1,38 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class LoginPage extends BasePage {
+    private final By usernameField = By.id("user-name");
+    private final By passwordField = By.id("password");
+    private final By loginButton = By.id("login-button");
+    private final By errorMessage = By.xpath("login_button_container h3");
+
+    //Set method
+    public void setUsername(String username) {
+        set(usernameField, username);
+    }
+
+    //Set method
+    public void setPassword(String password) {
+        set(passwordField, password);
+    }
+
+    //Transition method
+    public ProductsPage clickLoginButton() {
+        click(loginButton);
+        return new ProductsPage();
+    }
+
+    //Convenience method
+    public ProductsPage logIntoApplication(String username, String password) {
+        setUsername(username);
+        setPassword(password);
+        return clickLoginButton();
+    }
+
+    //Get method
+    public String getErrorMessage() {
+        return find(errorMessage).getText();
+    }
+}
---
+++ page-object-model-design-pattern/src/main/java/pages/ProductsPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class ProductsPage extends BasePage {
+    private final By productsHeader = By.xpath("//span[text()='Products']");
+
+    public boolean isProductsHeaderDisplayed() {
+        return find(productsHeader).isDisplayed();
+    }
+}
