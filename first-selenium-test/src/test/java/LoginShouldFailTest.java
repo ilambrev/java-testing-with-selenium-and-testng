1efc6e59bab22250a3f5d4fb1ff00076e45df563
ilambrev <ilambrev@abv.bg>
2024-10-28 09:36:52 +0200

    Login should fail test

---
+++ first-selenium-test/src/test/java/LoginShouldFailTest.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginShouldFailTest {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit(); //closes every window of browser and quits the driver (Standard for Selenium)
+        //driver.close(); //closes only the current window
+    }
+
+    @Test
+    public void testLoginIntoApplication() throws InterruptedException {
+        Thread.sleep(2000);
+
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("Admin");
+
+        var password = driver.findElement(By.name("password"));
+        password.sendKeys("admin123");
+
+        driver.findElement(By.tagName("button")).click();
+
+        Thread.sleep(2000); //for demo
+
+        String actualResult = driver.findElement(By.tagName("h6")).getText();
+        String expectedResult = "Dashboard";
+
+        Assert.assertNotEquals(actualResult, expectedResult);
+    }
+}
