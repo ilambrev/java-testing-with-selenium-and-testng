06b2f8a710de634f1da9b53793bb69826a00c10b
ilambrev <ilambrev@abv.bg>
2024-11-01 20:58:11 +0200

    Test Radio Button

--- working-with-web-elements/src/main/java/pages/forms/FormsPage.java
+++ working-with-web-elements/src/main/java/pages/forms/FormsPage.java
@@ -1,7 +1,17 @@
 package pages.forms;
 
+import org.openqa.selenium.By;
 import pages.HomePage;
 
-public class FormsPage extends HomePage {
+import static utilities.JavaScriptUtility.scrollToElementJS;
 
+public class FormsPage extends HomePage {
+    private final By practiceFormMenuItem = By.xpath("//li[@id='item-0']/span[text()='Practice Form']");
+
+    public PracticeFormPage clickPracticeForm() {
+        scrollToElementJS(practiceFormMenuItem);
+        click(practiceFormMenuItem);
+
+        return new PracticeFormPage();
+    }
 }
---
+++ working-with-web-elements/src/test/java/tests/forms/RadioButtonTest.java
@@ -0,0 +1,17 @@
+package tests.forms;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RadioButtonTest extends BaseTest {
+
+    @Test
+    public void testRadioButton() {
+        var formsPage = homePage.goToForms().clickPracticeForm();
+        formsPage.clickFemaleRadioButton();
+        boolean isFemaleRadioButtonSelected = formsPage.isFemaleRadioButtonSelected();
+
+        Assert.assertTrue(isFemaleRadioButtonSelected, "Female Radio Button Is Not Selected");
+    }
+}
