87c539ab3d61c33550efcdfa42d2f3b12b982a89
ilambrev <ilambrev@abv.bg>
2024-11-02 16:25:09 +0200

    Test Checkbox

--- working-with-web-elements/src/main/java/pages/forms/PracticeFormPage.java
+++ working-with-web-elements/src/main/java/pages/forms/PracticeFormPage.java
@@ -6,7 +6,10 @@ import static utilities.JavaScriptUtility.clickJS;
 import static utilities.JavaScriptUtility.scrollToElementJS;
 
 public class PracticeFormPage extends FormsPage {
-    private By femaleRadioButton = By.id("gender-radio-2");
+    private final By femaleRadioButton = By.id("gender-radio-2");
+    private final By sportsHobbyCheckbox = By.id("hobbies-checkbox-1");
+    private final By readingHobbyCheckbox = By.id("hobbies-checkbox-2");
+    private final By musicHobbyCheckbox = By.id("hobbies-checkbox-3");
 
     public void clickFemaleRadioButton() {
         scrollToElementJS(femaleRadioButton);
@@ -15,5 +18,37 @@ public class PracticeFormPage extends FormsPage {
 
     public boolean isFemaleRadioButtonSelected() {
         return find(femaleRadioButton).isSelected();
+    }
+
+    public void clickSportsHobbyCheckbox() {
+        if (!find(sportsHobbyCheckbox).isSelected()) {
+            scrollToElementJS(sportsHobbyCheckbox);
+            clickJS(sportsHobbyCheckbox);
+        }
+    }
+
+    public void clickReadingHobbyCheckbox() {
+        if (!find(readingHobbyCheckbox).isSelected()) {
+            scrollToElementJS(readingHobbyCheckbox);
+            clickJS(readingHobbyCheckbox);
+        }
+    }
+
+    public void clickMusicHobbyCheckbox() {
+        if (!find(musicHobbyCheckbox).isSelected()) {
+            scrollToElementJS(musicHobbyCheckbox);
+            clickJS(musicHobbyCheckbox);
+        }
+    }
+
+    public void unClickReadingHobbyCheckbox() {
+        if (find(readingHobbyCheckbox).isSelected()) {
+            scrollToElementJS(readingHobbyCheckbox);
+            clickJS(readingHobbyCheckbox);
+        }
+    }
+
+    public boolean isReadingHobbyCheckboxSelected() {
+        return find(readingHobbyCheckbox).isSelected();
     }
 }
---
+++ working-with-web-elements/src/test/java/tests/forms/CheckboxTest.java
@@ -0,0 +1,20 @@
+package tests.forms;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CheckboxTest extends BaseTest {
+
+    @Test
+    public void testCheckbox() {
+        var formsPage = homePage.goToForms().clickPracticeForm();
+        formsPage.clickSportsHobbyCheckbox();
+        formsPage.clickReadingHobbyCheckbox();
+        formsPage.clickMusicHobbyCheckbox();
+        formsPage.unClickReadingHobbyCheckbox();
+
+        boolean isReadingHobbyCheckboxSelected = formsPage.isReadingHobbyCheckboxSelected();
+        Assert.assertFalse(isReadingHobbyCheckboxSelected, "Reading Hobby Checkbox Is Selected");
+    }
+}
